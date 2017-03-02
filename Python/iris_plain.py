c21bd19187849c4f4a7d29cd016694291d915872
Rian Adam <rian.adam.r@gmail.com>
2017-03-02 21:34:44 +0700

    improved hyperparameter and naming variable

--- Python/iris_plain.py
+++ Python/iris_plain.py
@@ -15,13 +15,13 @@ with open('../Datasets/iris/iris.csv') as csvfile:
 
 # Change string value to numeric
 for row in dataset:
-    row[4]=["Iris-setosa", "Iris-versicolor", "Iris-virginica"].index(row[4])
-    row[:4]=[float(row[j]) for j in xrange(len(row))]
+    row[4] = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"].index(row[4])
+    row[:4] = [float(row[j]) for j in xrange(len(row))]
 
 # Split x and y (feature and target)
 random.shuffle(dataset)
-datatrain = dataset[:int(len(dataset)*0.8)]
-datatest = dataset[int(len(dataset)*0.8):]
+datatrain = dataset[:int(len(dataset) * 0.8)]
+datatest = dataset[int(len(dataset) * 0.8):]
 train_X = [data[:4] for data in datatrain]
 train_y = [data[4] for data in datatrain]
 test_X = [data[:4] for data in datatest]
@@ -37,8 +37,10 @@ output layer : 3 neuron, represents the class of Iris
 
 optimizer = gradient descent
 loss function = Square ROot Error
-learning rate = 0.01
-epoch = 500
+learning rate = 0.005
+epoch = 400
+
+best result = 96.67%
 """
 
 def matrix_mul_bias(A, B, bias): # Fungsi perkalian matrix + bias (untuk Testing)
@@ -50,7 +52,7 @@ def matrix_mul_bias(A, B, bias): # Fungsi perkalian matrix + bias (untuk Testing
             C[i][j] += bias[j]
     return C
 
-def vec_mat_bias(A, B, bias=None): # Fungsi perkalian vector dengan matrix + bias
+def vec_mat_bias(A, B, bias): # Fungsi perkalian vector dengan matrix + bias
     C = [0 for i in xrange(len(B[0]))]
     for j in xrange(len(B[0])):
         for k in xrange(len(B)):
@@ -64,7 +66,6 @@ def mat_vec(A, B): # Fungsi perkalian matrix dengan vector (untuk backprop)
     for i in xrange(len(A)):
         for j in xrange(len(B)):
             C[i] += A[i][j] * B[j]
-
     return C
 
 def sigmoid(A, deriv=False): # Fungsi aktivasi sigmoid
@@ -77,27 +78,27 @@ def sigmoid(A, deriv=False): # Fungsi aktivasi sigmoid
     return A
 
 # Define parameter
-alfa = 0.01
-epoch = 500
-neuron = [4, 3, 3] # arsitektur tiap layer
+alfa = 0.005
+epoch = 400
+neuron = [4, 4, 3] # arsitektur tiap layer
 
 # Initiate weight and bias with 0 value
 weight = [[0 for j in xrange(neuron[1])] for i in xrange(neuron[0])]
 weight_2 = [[0 for j in xrange(neuron[2])] for i in xrange(neuron[1])]
-bias = [0 for _ in xrange(neuron[1])]
-bias_2 = [0 for _ in xrange(neuron[2])]
+bias = [0 for i in xrange(neuron[1])]
+bias_2 = [0 for i in xrange(neuron[2])]
 
 # Initiate weight with random between -1.0 ... 1.0
 for i in xrange(neuron[0]):
     for j in xrange(neuron[1]):
-        weight[i][j] = 2*random.random()-1
+        weight[i][j] = 2 * random.random() - 1
 
 for i in xrange(neuron[1]):
     for j in xrange(neuron[2]):
-        weight_2[i][j] = 2*random.random()-1
+        weight_2[i][j] = 2 * random.random() - 1
 
 
-for _ in xrange(epoch):
+for e in xrange(epoch):
     cost_total = 0
     for idx, x in enumerate(train_X): # Update for each data; SGD
         
@@ -109,37 +110,37 @@ for _ in xrange(epoch):
         
         # Convert to One-hot target
         target = [0, 0, 0]
-        target[int(train_y[idx])]=1
+        target[int(train_y[idx])] = 1
 
         # Cost function, Square Root Eror
         eror = 0
         for i in xrange(3):
-            eror +=  0.5*(target[i] - X_2[i])**2 
+            eror +=  0.5 * (target[i] - X_2[i]) ** 2 
         cost_total += eror
 
         # Backward propagation
         # Update weight_2 and bias_2 (layer 2)
         delta_2 = []
         for j in xrange(neuron[2]):
-            delta_2.append(-1 * (target[j]-X_2[j]) * X_2[j]*(1-X_2[j]))
+            delta_2.append(-1 * (target[j]-X_2[j]) * X_2[j] * (1-X_2[j]))
 
         for i in xrange(neuron[1]):
             for j in xrange(neuron[2]):
-                weight_2[i][j] -= alfa*(delta_2[j] * X_1[i])
-                bias_2[j] -= alfa*delta_2[j]
+                weight_2[i][j] -= alfa * (delta_2[j] * X_1[i])
+                bias_2[j] -= alfa * delta_2[j]
         
         # Update weight and bias (layer 1)
         delta_1 = mat_vec(weight_2, delta_2)
         for j in xrange(neuron[1]):
-            delta_1[j] = delta_1[j] * (X_1[j]*(1-X_1[j]))
+            delta_1[j] = delta_1[j] * (X_1[j] * (1-X_1[j]))
         
         for i in xrange(neuron[0]):
             for j in xrange(neuron[1]):
-                weight[i][j] -=  alfa*(delta_1[j] * x[i])
-                bias[j] -= alfa*delta_1[j]
+                weight[i][j] -=  alfa * (delta_1[j] * x[i])
+                bias[j] -= alfa * delta_1[j]
     
     cost_total /= len(train_X)
-    if(_%100==0):
+    if(e % 100 == 0):
         print cost_total # Print cost untuk memantau training
 
 """
@@ -160,6 +161,6 @@ print preds
 # Calculate accuration
 acc = 0.0
 for i in xrange(len(preds)):
-    if preds[i]==int(test_y[i]):
+    if preds[i] == int(test_y[i]):
         acc += 1
-print acc / len(preds)*100, "%"
+print acc / len(preds) * 100, "%"

68478789d35a27e0d8783dbba074f67defbd048b
rian <adamuslim.juga@gmail.com>
2017-09-21 02:46:07 +0700

    added pytorch

--- Pytorch/iris_pytorch.py
+++ Pytorch/iris_pytorch.py
@@ -29,32 +29,58 @@ SECTION 2 : Build and Train Model
 Multilayer perceptron model, with one hidden layer.
 input layer : 4 neuron, represents the feature of Iris
 hidden layer : 10 neuron, activation using ReLU
-output layer : 3 neuron, represents the class of Iris, Softmax Layer
+output layer : 3 neuron, represents the class of Iris
 
 optimizer = stochastic gradient descent with no batch-size
 loss function = categorical cross entropy
-learning rate = default from keras.optimizer.SGD, 0.01
+learning rate = 0.01
 epoch = 500
 """
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.utils import np_utils
 
-#change target format
-ytrain = np_utils.to_categorical(ytrain) 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+torch.manual_seed(1234)
+
+#hyperparameters
+hl = 10
+lr = 0.01
+num_epoch = 500
 
 #build model
-model = Sequential()
-model.add(Dense(output_dim=10, input_dim=4))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=3))
-model.add(Activation("softmax"))
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(4, hl)
+        self.fc2 = nn.Linear(hl, 3)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+net = Net()
 
 #choose optimizer and loss function
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=lr)
 
 #train
-model.fit(xtrain, ytrain, nb_epoch=500, batch_size=120)
+for epoch in range(num_epoch):
+    X = Variable(torch.Tensor(xtrain).float())
+    Y = Variable(torch.Tensor(ytrain).long())
+
+    #feedforward - backprop
+    optimizer.zero_grad()
+    out = net(X)
+    loss = criterion(out, Y)
+    loss.backward()
+    optimizer.step()
+
+    if (epoch) % 50 == 0:
+        print ('Epoch [%d/%d] Loss: %.4f' 
+                   %(epoch+1, num_epoch, loss.data[0]))
 
 """
 SECTION 3 : Testing model
@@ -76,14 +102,10 @@ xtest = datatest_array[:,:4]
 ytest = datatest_array[:,4]
 
 #get prediction
-classes = model.predict_classes(xtest, batch_size=120)
+X = Variable(torch.Tensor(xtest).float())
+Y = torch.Tensor(ytest).long()
+out = net(X)
+_, predicted = torch.max(out.data, 1)
 
 #get accuration
-import numpy as np
-accuration = np.sum(classes == ytest)/30.0 * 100
-
-print "Test Accuration : " + str(accuration) + '%'
-print "Prediction :"
-print classes
-print "Target :"
-print np.asarray(ytest,dtype="int32")
+print('Accuracy of the network %d %%' % (100 * torch.sum(Y==predicted) / 30))
--- README.md
+++ README.md
@@ -12,8 +12,12 @@ This is a collection of simple and easy to read program for Iris dataset classif
 - [Simple Neural Net for Iris dataset without external library][iris_plain_2] (No-hidden layer model)
 - [Simple Neural Net for Iris dataset using Scikit-learn-MLPClassifier][iris_scikit] (Multilayer perceptron model, with one hidden layer)
 
+## PyTorch
+- [Simple Neural Net for Iris dataset using PyTorch][iris_pytorch] (Multilayer perceptron model, with one hidden layer)
+
 [iris_keras]:https://github.com/rianrajagede/iris-python/blob/master/Keras/iris_keras.py
 [iris_lasagne]:https://github.com/rianrajagede/iris-python/blob/master/Lasagne/iris_lasagne.py
 [iris_scikit]:https://github.com/rianrajagede/iris-python/blob/master/Python/iris_scikit.py
 [iris_plain]:https://github.com/rianrajagede/iris-python/blob/master/Python/iris_plain_mlp.py
 [iris_plain_2]:https://github.com/rianrajagede/iris-python/blob/master/Python/iris_plain_slp.py
+[iris_pytorch]:https://github.com/rianrajagede/iris-python/blob/master/Pytorch/iris_pytorch.py

c53ffaed403f67e3c4ee1a65d51dba4fb18dc0a8
rianrajagede <rian.adam.r@gmail.com>
2019-08-01 17:45:36 +0700

    fixed: bias calculation error

--- Python/iris_plain_slp.py
+++ Python/iris_plain_slp.py
@@ -56,7 +56,7 @@ def vec_mat_bias(A, B, bias): # Vector (A) x matrix (B) multiplication
     for j in range(len(B[0])):
         for k in range(len(B)):
             C[j] += A[k] * B[k][j]
-            C[j] += bias[j]
+        C[j] += bias[j]
     return C
 
 
